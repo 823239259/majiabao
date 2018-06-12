0a29630eb8310ae1a14eba2d7c963fd492f12667
chenbeisheng <cbeisheng_work@foxmail.com>
2018-06-12 15:14:10 +0800

    222

---
+++ everyone-futures/src/page/quote.vue
@@ -0,0 +1,21 @@
+<template>
+	<div id="quote">
+		<h1>哈哈哈</h1>
+	</div>
+</template>
+
+<script>
+	export default{
+		name:"",
+		data(){
+			return{
+				
+			}
+		}
+	}
+</script>
+
+
+<style lang="scss" scoped>
+	@import "../assets/css/common.scss";
+</style>
