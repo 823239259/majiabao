2da96f26470b222036251790acbc700586bcfa03
kelele <823239259@qq.com>
2018-06-20 17:08:18 +0800

    111

--- everyone-futures/src/page/quote/presentation.vue
+++ everyone-futures/src/page/quote/presentation.vue
@@ -31,6 +31,7 @@
 	let echarts = require('echarts/lib/echarts')
 	// 引入柱状图组件
 	require('echarts/lib/chart/line')
+	require('echarts/lib/chart/bar')
 	// 引入提示框和title组件
 	require('echarts/lib/component/tooltip')
 	require('echarts/lib/component/title')
