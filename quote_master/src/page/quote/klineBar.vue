af0524015716d5be755c8dd7c981cd95a31a717b
chenbeisheng <cbeisheng_work@foxmail.com>
2018-08-15 15:45:21 +0800

    222

---
+++ quote_master/src/page/quote/klineBar.vue
@@ -0,0 +1,54 @@
+<template>
+	<div id="echarts">
+		<div id="kline"></div>
+		<div id="kline_volume"></div>
+	</div>
+</template>
+
+<script>
+	export default{
+		name: 'echarts',
+		computed: {
+			quoteSocket(){
+				return this.$store.state.quoteSocket;
+			},
+			currentdetail(){
+				return this.$store.state.market.currentdetail;
+			}
+		},
+		mounted: function(){
+			this.$store.state.isshow.iskline = true;
+			this.$store.state.market.selectTime = 1;
+			var data = {
+				Method: "QryHistory",
+				Parameters:{
+					ExchangeNo: this.currentdetail.ExchangeNo,
+					CommodityNo: this.currentdetail.CommodityNo,
+					ContractNo: this.currentdetail.MainContract,
+					HisQuoteType: 1,
+					BeginTime: "",
+					EndTime: "",
+					Count: 0
+				}
+			};
+			this.quoteSocket.send(JSON.stringify(data));
+		},
+		
+		
+	}
+</script>
+
+<style lang="scss" scoped>
+	@import "../../assets/css/common.scss";
+	#fens, #volume{
+		width: 100%;
+		margin: 0 auto;
+	}
+	#kline{
+		height: 2.5rem;
+	}
+	#kline_volume{
+		height:4rem;
+	}
+	
+</style>
